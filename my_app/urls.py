72649ff8ee219655d7b313fa7a5b548594f788f0
RaktimParasar <raktimparasar96@gmail.com>
2020-02-02 19:01:59 +0530

    Update

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'my_app'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('my_app/', views.index, name='index'),
     path('search_result', views.search_result, name='search_result')
 ]
